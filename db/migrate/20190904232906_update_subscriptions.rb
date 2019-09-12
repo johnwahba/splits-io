d0b5aac58d6138a5f29ea9669019800f22f0b161
glacials <qhiiyr@gmail.com>
2019-09-12 14:42:06 -0700

    Allow the subscriptions table migration to be reversed

--- db/migrate/20190904232906_update_subscriptions.rb
+++ db/migrate/20190904232906_update_subscriptions.rb
@@ -1,7 +1,13 @@
 class UpdateSubscriptions < ActiveRecord::Migration[6.0]
   def change
     # Old subscriptions table is from 2016 and does not use UUIDs
-    drop_table :subscriptions
+    drop_table :subscriptions do |t|
+      t.belongs_to :user, index: true
+      t.string :stripe_subscription_id
+      t.string :stripe_plan_id
+      t.string :stripe_customer_id
+    end
+
     create_table :subscriptions, id: :uuid do |t|
       t.belongs_to :user, index: true
       t.string :stripe_session_id
